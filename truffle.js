52f89130a7dc8eb8e5052276a7f69bf8dbbd42b1
Tarek Sakr <tarek@dropmi.com>
2018-03-02 12:47:20 -0500

    Configured Truffle networks

--- truffle.js
+++ truffle.js
@@ -1,4 +1,17 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/");
+      },
+      network_id: '4',
+      from: "0x40f09c2272e7e1928874f0463bedcecde8bc0cb2",
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
 };
